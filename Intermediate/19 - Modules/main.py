6bfc6bf3fbddb6a210d053356406b7a2974d3985
bipinkalra-scaler <bipin.kalra@scaler.com>
2022-12-07 09:22:07 +0530

    Modules

---
+++ Intermediate/19 - Modules/main.py
@@ -0,0 +1,5 @@
+print("Current File -", __name__)
+
+import module_1
+
+print("Module File -", module_1.__name__)
---
+++ Intermediate/19 - Modules/module_1.py
@@ -0,0 +1,4 @@
+def cool_func():
+	print("I am so cool!")
+
+print("Current File (Module 1) -", __name__)
